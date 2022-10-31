b11d9049a919d28fea6f58ee89ba6c5cf7d78a8a
SHEF <Shefqetmush@gmail.com>
2022-10-31 13:31:48 -0400

    submit project stylings & code

--- src/project/projectSubmit.js
+++ src/project/projectSubmit.js
@@ -1,12 +1,17 @@
 export function projectSubmit() {
-  const projectButton = document.querySelector(".project-Button");
+  const form = document.querySelector(".projects-Form");
   //createdProjects
-  projectButton.addEventListener("click", () => {
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
     const inp = document.querySelector("#project").value;
-    const createProjectsDiv = document.querySelector(".createdProjects");
+    const createProjectsDiv = document.querySelector(".created-Projects");
     const p = document.createElement("p");
 
     p.innerHTML = inp;
     createProjectsDiv.appendChild(p);
+
+    form.reset();
   });
 }
