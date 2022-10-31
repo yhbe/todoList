5018e1460b64cbf051c0472dbe3c0733e6b90aed
SHEF <Shefqetmush@gmail.com>
2022-10-31 13:52:13 -0400

    added project Factory and clear()

--- src/project/projectSubmit.js
+++ src/project/projectSubmit.js
@@ -1,17 +1,37 @@
+let list = [];
+
+function clear() {
+  const createProjectsDiv = document.querySelector(".created-Projects");
+  createProjectsDiv.innerHTML = "";
+}
+
+function CreateProject(project) {
+  this.name = project;
+  this.id = Math.random();
+}
+
 export function projectSubmit() {
   const form = document.querySelector(".projects-Form");
   //createdProjects
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
+    clear();
 
     const inp = document.querySelector("#project").value;
     const createProjectsDiv = document.querySelector(".created-Projects");
-    const p = document.createElement("p");
 
-    p.innerHTML = inp;
-    createProjectsDiv.appendChild(p);
+    list.push(new CreateProject(inp));
+    list.forEach((project) => {
+      const p = document.createElement("p");
+      p.classList.add(project.id);
+      p.innerHTML = project.name;
+      createProjectsDiv.append(p);
+    });
+    console.log(list);
 
     form.reset();
   });
 }
+
+function createProjectIcons() {}
