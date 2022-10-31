0d2b4cf5882603b37a820a8e7c6fc9fdbc144e29
SHEF <Shefqetmush@gmail.com>
2022-10-31 15:01:32 -0400

    added trashEventListener function filter array

--- src/project/projectSubmit.js
+++ src/project/projectSubmit.js
@@ -22,23 +22,50 @@ export function projectSubmit() {
     const inp = document.querySelector("#project").value;
 
     list.push(new CreateProject(inp));
-    list.forEach((project) => {
-      const p = document.createElement("p");
-      p.classList.add(project.id);
-      p.classList.add("single-Project");
-      p.innerHTML = project.name;
-      createProjectIcons(p);
-    });
+    loadProjectDom();
     console.log(list);
 
     form.reset();
+  });
+}
+function loadProjectDom() {
+  list.forEach((project) => {
+    const p = document.createElement("p");
+    p.classList.add(project.id);
+    p.classList.add("single-Project");
+    p.innerHTML = project.name;
+    createProjectIcons(p);
   });
 }
 
 function createProjectIcons(p) {
   const createProjectsDiv = document.querySelector(".created-Projects");
   const img = document.createElement("img");
+  img.classList.add("remove-Project");
   img.src = circle;
   p.appendChild(img);
   createProjectsDiv.append(p);
+  trashEventListener();
+}
+
+function trashEventListener() {
+  const trashImages = document.querySelectorAll(".remove-Project");
+
+  trashImages.forEach((image) => {
+    image.addEventListener("click", (e) => {
+      const deletedProjectId = parseFloat(e.target.parentNode.classList[0]);
+
+      e.target.parentNode.remove();
+
+      let filteredList = [];
+
+      list.filter((item) => {
+        if (item.id !== deletedProjectId) {
+          filteredList.push(item);
+        }
+      });
+
+      list = filteredList;
+    });
+  });
 }
